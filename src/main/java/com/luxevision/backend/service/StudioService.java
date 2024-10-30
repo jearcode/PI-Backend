7996cdb84bed6cad839e178b255232c54d7459c2
jearcode <jereibarra10@outlook.es>
2024-10-30 07:56:32 -0500

    feat: complete CRUD functionality in StudioService

--- src/main/java/com/luxevision/backend/service/StudioService.java
+++ src/main/java/com/luxevision/backend/service/StudioService.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudioService {
@@ -15,5 +16,25 @@ public class StudioService {
 
     public List<Studio> getRandomStudios() {
         return studioRepository.findRandomStudios(PageRequest.of(0, 10));
+    }
+
+    public Studio saveStudio (Studio studio) {
+        return studioRepository.save(studio);
+    }
+
+    public Optional<Studio> findStudioById (Integer id) {
+        return studioRepository.findById(id);
+    }
+
+    public Studio updateStudio (Studio studio) {
+        return studioRepository.save(studio);
+    }
+
+    public void deleteStudioById (Integer id) {
+        studioRepository.deleteById(id);
+    }
+
+    public List<Studio> findAllStudios() {
+        return studioRepository.findAll();
     }
 }
