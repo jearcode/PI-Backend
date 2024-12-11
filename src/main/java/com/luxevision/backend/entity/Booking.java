aa533912d1c8640b505e09975a302301e42101c3
jearcode <jereibarra10@outlook.es>
2024-12-10 22:15:43 -0500

    refactor booking entity: added nullable constraints, renamed booking_status column

--- src/main/java/com/luxevision/backend/entity/Booking.java
+++ src/main/java/com/luxevision/backend/entity/Booking.java
@@ -15,34 +15,39 @@ import java.time.LocalTime;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "bookings")
 public class Booking {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "booking_date")
+    @Column(name = "booking_date", nullable = false)
     private LocalDate date;
-    @Column(name = "start_time")
+
+    @Column(name = "start_time", nullable = false)
     private LocalTime startTime;
-    @Column(name = "end_time")
+
+    @Column(name = "end_time", nullable = false)
     private LocalTime endTime;
-    @Column(name = "total_price")
+
+    @Column(name = "total_price", nullable = false)
     private Double totalPrice;
 
+    @Column(name = "booking_status", nullable = false)
     @Enumerated(EnumType.STRING)
     private Status status;
 
     @ManyToOne
-    @JoinColumn(name = "studio_id")
+    @JoinColumn(name = "studio_id", nullable = false)
     private Studio studio;
 
     @ManyToOne
-    @JoinColumn(name = "specialty_id")
+    @JoinColumn(name = "specialty_id", nullable = false)
     private Specialty specialty;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
 }
